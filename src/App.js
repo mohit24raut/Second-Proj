5c36b7397a348a1b3ef2394f7b5496a21fe11bd9
Mohit Raut <rautm3636@gmail.com>
2023-06-13 15:40:39 +0530

    React interpolation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+function App()
+{
+  return(
+    <>
+    <h1>Hello World</h1>
+    <h1>Student</h1>
+    </>
+  )
 }
+
+function App()
+{
+  let jsx=<h1>Hello World</h1>;
+  return jsx;
+}
+
+function App()
+{
+  let jsx=<h1>Hello World</h1>;
+  let jsx1=(
+    <>
+      <h1>Hello CDAC</h1>
+    </>
+  )
+
+  let bool=false;
+  return bool?jsx:jsx1;
+}
+
+------------------------------------------------------------------------------------
+Interpolation Concept
+
+function App()
+{
+  let str=`Hello CDAC`;
+  let str2='Hello Student';
+  return (
+    <>
+      <h1>{str.toLowerCase()}</h1>
+      <h1>{str.toUpperCase()}</h1>
+      <h1>{str2.substring(2)}</h1>
+      <h1>{str.charAt(2)}</h1>
+    </>
+  )
+}
+
+function App()
+{
+  let pi=3.14;
+  let bool= true;
+  return (
+    <>
+      <h1>PI={pi}</h1>
+      <h1>{bool?"Good one":"Bad One"}</h1>
+    </>
+  )
+}*/
+
 
 export default App;
