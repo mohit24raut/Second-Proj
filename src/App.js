a785e8395afe4d92e60c4c02859d0c47edff0c52
Mohit Raut <rautm3636@gmail.com>
2023-06-13 15:53:25 +0530

    Use of stylimg

--- src/App.js
+++ src/App.js
@@ -1,61 +1,24 @@
 /*
+Styling by creating object literals
+
+function App()
+{
+  let style={backgroundColor:"green", color:"white"};
+  return(
+    <>
+    <h1 style={style}>Hello</h1>
+    </>
+  )
+}
+*/
+import "./App.css";
+
 function App()
 {
   return(
     <>
-    <h1>Hello World</h1>
-    <h1>Student</h1>
+    <h1 className="react">Hello</h1>
     </>
   )
 }
-
-function App()
-{
-  let jsx=<h1>Hello World</h1>;
-  return jsx;
-}
-
-function App()
-{
-  let jsx=<h1>Hello World</h1>;
-  let jsx1=(
-    <>
-      <h1>Hello CDAC</h1>
-    </>
-  )
-
-  let bool=false;
-  return bool?jsx:jsx1;
-}
-
-------------------------------------------------------------------------------------
-Interpolation Concept
-
-function App()
-{
-  let str=`Hello CDAC`;
-  let str2='Hello Student';
-  return (
-    <>
-      <h1>{str.toLowerCase()}</h1>
-      <h1>{str.toUpperCase()}</h1>
-      <h1>{str2.substring(2)}</h1>
-      <h1>{str.charAt(2)}</h1>
-    </>
-  )
-}
-
-function App()
-{
-  let pi=3.14;
-  let bool= true;
-  return (
-    <>
-      <h1>PI={pi}</h1>
-      <h1>{bool?"Good one":"Bad One"}</h1>
-    </>
-  )
-}*/
-
-
 export default App;
